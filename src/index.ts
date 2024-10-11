319f23e14f3a94596fd82f85219cddd59f1d770b
Isaac Batista <isaacbatst@gmail.com>
2024-10-11 13:00:03 -0300

    feat: add session with join code

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
 import cors from "cors";
+import dotenv from 'dotenv';
 import express from "express";
 import 'express-async-errors';
-import { Document, Filter, MongoClient, ObjectId } from "mongodb";
-import dotenv from 'dotenv';
 import session, { SessionOptions } from 'express-session';
+import { Document, Filter, MongoClient, ObjectId } from "mongodb";
+import * as crypto from 'crypto';
 dotenv.config();
 
 const env = process.env.NODE_ENV || 'development';
+const corsOrigins = process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : ['http://127.0.0.1:5500', 'http://localhost:5500']
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017';
 const mongoDbName = process.env.MONGO_DB_NAME || 'pelada';
-const domain = process.env.DOMAIN || 'localhost';
+const domain = process.env.COOKIE_DOMAIN || '';
 
 const client = new MongoClient(mongoUrl);
 const db = client.db(mongoDbName);
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: corsOrigins,
+  credentials: true
+}));
 app.use(express.json());
 
 const sessionOptions: SessionOptions = {
@@ -28,7 +33,6 @@ const sessionOptions: SessionOptions = {
     httpOnly: true,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     path: '/',
-    sameSite: 'none',
   }
 }
 
@@ -51,28 +55,31 @@ app.get('/game-days', async (req, res) => {
 })
 
 app.post('/game-days', async (req, res) => {
+  const joinCode = crypto.randomBytes(2).toString('hex').toUpperCase();
   const created = await db.collection('game-days').insertOne({
     ...req.body,
+    joinCode,
     playedOn: new Date(req.body.playedOn),
   });
+
+  req.session.gameDayId = created.insertedId.toHexString();
+
   res.status(201).json({
     id: created.insertedId,
+    joinCode,
   });
 })
 
-app.put('/game-days/:id', async (req, res) => {
-  const id = req.params.id;
-  const result = await db.collection('game-days').updateOne({
-    _id: new ObjectId(id)
-  },
-  {
-    $set: req.body
+app.put('/game-days/join/:code', async (req, res) => {
+  const gameDay = await db.collection('game-days').findOne({
+    joinCode: req.params.code
   });
-  if(result.matchedCount === 0) {
+  if (!gameDay) {
     res.status(404).end();
     return;
   }
-  res.status(200).end();
+  req.session.gameDayId = gameDay._id.toHexString();
+  res.status(200).json(gameDay)
 });
 
 app.put('/players', async (req, res) => {
@@ -119,6 +126,47 @@ app.put('/players/bulk', async (req, res) => {
   await bulk.execute();
   res.status(200).end();
 })
+
+
+app.get('/sessions/game-day', async (req, res) => {
+  const id = req.session.gameDayId;
+  console.log('session', req.session)
+  if (!id) {
+    res.status(404).end();
+    return;
+  }
+  const gameDay = await db.collection('game-days').findOne({
+    _id: new ObjectId(id),
+    isLive: true,
+  });
+  if (!gameDay) {
+    res.status(404).end();
+    return;
+  }
+  res.json({
+    id,
+    ...gameDay,
+  });
+});
+app.put('/sessions/game-day', async (req, res) => {
+  const id = req.session.gameDayId;
+  if (!id) {
+    res.status(404).end();
+    return;
+  }
+  const result = await db.collection('game-days').updateOne({
+    _id: new ObjectId(id)
+  },
+  {
+    $set: req.body
+  });
+  if(result.matchedCount === 0) {
+    res.status(404).end();
+    return;
+  }
+  res.status(200).end();
+});
+
 
 async function main() {
   try {
